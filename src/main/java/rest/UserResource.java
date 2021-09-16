03aee8f1b62215bd65800df97aa2fd1cfb7f73d4
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-09-16 10:29:57 +0200

    Modif Ressources : EntityManagerHelper

--- src/main/java/rest/UserResource.java
+++ src/main/java/rest/UserResource.java
@@ -1,13 +1,9 @@
-package fr.istic.taa.jaxrs.rest;
+package rest;
 
 import dao.UtilisateurDao;
 import domain.Utilisateur;
-import fr.istic.taa.jaxrs.domain.Pet;
 import io.swagger.v3.oas.annotations.Parameter;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
@@ -15,26 +11,28 @@ import javax.ws.rs.core.Response;
 @Produces({"application/json", "application/xml"})
 public class UserResource {
 
-
     @GET
     @Path("/{userId}")
     public Utilisateur getUserById(@PathParam("userId") Long userId)  {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev");
-        EntityManager manager = factory.createEntityManager();
-        UtilisateurDao utilisateurDao = new UtilisateurDao(manager);
+        UtilisateurDao utilisateurDao = new UtilisateurDao();
         return utilisateurDao.searchUserById(userId);
     }
-
 
     @POST
     @Consumes("application/json")
     public Response addUser(
             @Parameter(description = "User that needs to be added", required = true) Utilisateur user) {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev");
-        EntityManager manager = factory.createEntityManager();
-        UtilisateurDao utilisateurDao = new UtilisateurDao(manager);
+        UtilisateurDao utilisateurDao = new UtilisateurDao();
         utilisateurDao.addUser(user);
         return Response.ok().entity(user).build();
+    }
+
+    @DELETE
+    @Path("/{userId}")
+    public Response deleteUserById(@PathParam("userId") Long userId)  {
+        UtilisateurDao userDao = new UtilisateurDao();
+        userDao.deleteUserById(userId);
+        return Response.ok().build();
     }
 
 }
